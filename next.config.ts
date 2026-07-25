1393ce79362fa87f4cc838bbbba8b38b6d461006
puffyinu <puffyinuofficial@gmail.com>
2026-07-25 19:32:45 +0000

    chore(docs): checkpoint before documentation cleanup

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,8 @@ const nextConfig: NextConfig = {
   allowedDevOrigins: [
     "3000-cs-ae9c0896-fa92-4c4d-b098-60d77c84b5d2.cs-asia-southeast1-kelp.cloudshell.dev",
     "3000-cs-89424410-f336-46bc-816f-3cf9294f086e.cs-asia-southeast1-palm.cloudshell.dev",
-  ],
+  "3000-cs-d432b8d4-96ba-48cf-a4b2-24c2578116af.cs-asia-southeast1-kelp.cloudshell.dev",
+],
 };
 
 export default nextConfig;
